54500f121c50ea6921cf965464204cd45f869c09
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-21 20:42:24 +0300

    радактирование файла

--- handlers/admin/add_questions.py
+++ handlers/admin/add_questions.py
@@ -8,11 +8,11 @@ from loader import dp, db
 
 @dp.message_handler(text="Добавить вопрос")
 async def stock_name(message: types.Message):
-    await message.answer("Введите вопрос")
-    await questions.name_questions.set()
+    await message.answer("Введите ответ")
+    await questions.answer.set()
 
 
-@dp.message_handler(state=questions.name_questions)
+@dp.message_handler(state=questions.answer)
 async def description_stock(message: types.Message, state: FSMContext):
     await state.update_data(name_questions=message.text)
     await questions.next()
@@ -23,19 +23,11 @@ async def description_stock(message: types.Message, state: FSMContext):
 @dp.message_handler(content_types=['photo'], state=questions.photo)
 async def description_adds_stock(message: types.Message, state: FSMContext):
     await state.update_data(photo=message.photo[0].file_id)
-    await questions.next()
-    await message.answer("Введите ответ")
-
-
-@dp.message_handler(state=questions.answer)
-async def description_stock(message: types.Message, state: FSMContext):
-    await state.update_data(answer=message.text)
 
     data_state = await state.get_data()
-    name = data_state['name_questions']
     photo = data_state['photo']
-    answer = data_state['answer']
+    answer = data_state['name_questions']
 
-    if await db.add_questions(photo, name, answer):
+    if await db.add_questions(photo, answer):
         await message.answer('Вы успешно добавили вопрос')
     await state.finish()
