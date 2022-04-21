9fd31708a2efd8b17e69a503c31f2dd48aca19b2
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-21 14:33:35 +0300

    install commit

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -20,33 +20,10 @@ async def bot_start(message: types.Message):
     else:
         await message.answer('Вы уже проходили данный тест\n'
                              'Вы прошли тест на "__баллы__"')
-    
+
 
 @dp.message_handler(commands=["create_database", "create_db", ])
 async def create_database(message: types.Message):
     await message.answer(text="База данных успешно создана!")
     await db.create_all_database()
 
-
-def get_answered_message(user):
-    question = db.get_question(user["question_index"])
-
-    text = f"Вопрос №{user['question_index'] + 1}\n\n{question['text']}\n"
-
-    for answer_index, answer in enumerate(question["answers"]):
-        text += f"{chr(answer_index + 97)}) {answer}"
-
-        if answer_index == question["correct"]:
-            text += " ✅"
-        elif answer_index == user["answers"][-1]:
-            text += " ❌"
-
-        text += "\n"
-
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.row(telebot.types.InlineKeyboardButton("Далее", callback_data="?next"))
-
-    return {
-        "text": text,
-        "keyboard": keyboard
-    }
