14513ff4750e70768b4e59c36606cd55263b9e69
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-21 20:43:26 +0300

    создание новой клавиатыр

--- keyboards/default/user/main_user_KD.py
+++ keyboards/default/user/main_user_KD.py
@@ -4,10 +4,23 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 async def main_kb() -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardMarkup(
         resize_keyboard=True,
-        one_time_keyboard=False,
+        one_time_keyboard=True,
         keyboard=[
             [
                 KeyboardButton(text="Начать прохождение теста")
+            ]
+        ]
+    )
+    return keyboard
+
+
+async def registr_user() -> ReplyKeyboardMarkup:
+    keyboard = ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        keyboard=[
+            [
+                KeyboardButton(text="Зарегистрироватся")
             ]
         ]
     )
