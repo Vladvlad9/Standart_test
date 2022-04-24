b973600ee43e8d15b6a005a704002d30fc034f67
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-24 22:20:15 +0300

    добавлена фонкция для пользователя

--- utils/db_api/db.py
+++ utils/db_api/db.py
@@ -31,6 +31,22 @@ class DBApi(object):
         result = f'SELECT is_passet FROM users WHERE user_id = {user_id}'
         return self.__cur.execute(result).fetchone()
 
+    async def get_first_question_users(self, user_id: int):
+        result = f'SELECT first_question FROM users WHERE user_id = {user_id}'
+        return self.__cur.execute(result).fetchone()
+
+    async def update_first_question_users(self, first_question: int, id_user: int):
+        """ADD """
+        self.__cur.execute('''
+                                UPDATE users
+                                SET first_question = ?
+                                WHERE user_id = ?
+                            ''', (first_question, id_user))
+        self.__conn.commit()
+
+    async def get_percent_users(self, user_id: int):
+        result = f'SELECT percent FROM users WHERE user_id = {user_id}'
+        return self.__cur.execute(result).fetchone()
 
     async def add_user(self, user_id, f_name: str, l_name: str, m_name: str, restaurant: str,
                        is_passet: str, correct_answer: int):
@@ -54,6 +70,15 @@ class DBApi(object):
         else:
             return True
 
+    async def update_percent_user(self, percent: int, id_user: int):
+        """ADD CORRECT_ANSWER"""
+        self.__cur.execute('''
+                                UPDATE users
+                                SET percent = ?
+                                WHERE user_id = ?
+                            ''', (percent, id_user))
+        self.__conn.commit()
+
 
     async def get_correct_answer_users(self, user_id: int):
         result = f'SELECT correct_answer FROM users WHERE user_id = {user_id}'
@@ -66,6 +91,10 @@ class DBApi(object):
 
     async def get_questions(self, id: int):
         result = f'SELECT * FROM questions Where id = {id}'
+        return self.__cur.execute(result).fetchall()
+
+    async def get_questions_id(self):
+        result = f'SELECT id FROM questions'
         return self.__cur.execute(result).fetchall()
 
     async def get_questions2(self, id: int):
@@ -89,7 +118,11 @@ class DBApi(object):
                             ''', (answer, id_user))
         self.__conn.commit()
 
-    async def update_wrong_answer(self, answer: int, id_user: int):
+    async def get_wrong_answers(self, id: int):
+        result = f'SELECT wrong_answers FROM users Where user_id = {id}'
+        return self.__cur.execute(result).fetchall()
+
+    async def update_wrong_answer(self, answer: list, id_user: int):
         """ADD CORRECT_ANSWER"""
         self.__cur.execute('''
                                 UPDATE users
