cf8efeed64f3fdd9eb1a98393f997f732b9c55b2
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-26 23:08:32 +0300

    удрал отчетво

--- states/profile.py
+++ states/profile.py
@@ -4,5 +4,4 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class ProfileSG(StatesGroup):
     first_name = State()
     last_name = State()
-    midle_name = State()
     restaraunt = State()
