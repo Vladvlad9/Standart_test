d73746b4a4edd80db78ae0133a8284361ddcc000
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-26 23:08:23 +0300

    удрал отчетво

--- handlers/users/add_new_user.py
+++ handlers/users/add_new_user.py
@@ -25,14 +25,6 @@ async def description_stock(message: types.Message, state: FSMContext):
 async def description_stock(message: types.Message, state: FSMContext):
     await state.update_data(last_name=message.text)
     await ProfileSG.next()
-    await ProfileSG.midle_name.set()
-    await message.answer("Введите ваше отчество")
-
-
-@dp.message_handler(state=ProfileSG.midle_name)
-async def description_stock(message: types.Message, state: FSMContext):
-    await state.update_data(midle_name=message.text)
-    await ProfileSG.next()
     await ProfileSG.restaraunt.set()
     await message.answer("Введите ваш ресторан в котором работаете")
 
@@ -44,12 +36,9 @@ async def description_stock(message: types.Message, state: FSMContext):
     data_state = await state.get_data()
     f_name = data_state['first_name']
     l_name = data_state['last_name']
-    m_name = data_state['midle_name']
     restaraunt = data_state['restaraunt']
 
-
-
-    if await db.add_user(message.from_user.id, f_name, l_name, m_name, restaraunt, 'Не прошел', 0):
+    if await db.add_user(message.from_user.id, f_name, l_name, restaraunt, 'Не прошел', 0):
         await message.answer('Вы успешно прошли регистрацию\n'
                              'Вам доступен тест для прохожедния\n'
                              'Для прохождения теста доступно 60 минут', reply_markup=await main_kb())
